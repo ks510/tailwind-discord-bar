227d77de3cc6470d98f07c8bebee432802e8e3b5
Karen <ksuen.dev@gmail.com>
2022-05-03 01:59:50 +0100

    ✨ NEW tooltip while hovering over icons in sidebar

--- src/SideBarIcon.js
+++ src/SideBarIcon.js
@@ -1,6 +1,9 @@
-const SideBarIcon = ({icon}) => (
-  <div className="sidebar-icon">
+const SideBarIcon = ({icon, text = 'tooltip'}) => (
+  <div className="sidebar-icon group">
     {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
   </div>
 );
 
