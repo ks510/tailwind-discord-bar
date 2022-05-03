8d329e469eb388a130768aa2437d4d94ba52001e
Karen <ksuen.dev@gmail.com>
2022-05-03 01:03:22 +0100

    💄 UI Discord theme colours added to config

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,6 +1,6 @@
 const SideBar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow">
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow">
       <i>A</i>
       <i>B</i>
       <i>C</i>
